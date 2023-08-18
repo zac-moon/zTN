8004fa89f28b8bea96f1f6f17dfcbe4406e446f6
lulu-moon <lulumoon2014@gmail.com>
2023-08-18 14:03:51 +0100

    added and fixed savefile feature

--- zTN.py
+++ zTN.py
@@ -6,4 +6,22 @@ root.title("My Notes")
 text = tk.Text(root)
 text.pack(fill=tk.BOTH, expand=True)
 
+def on_window_load():
+    print('window loaded')
+    try:
+        with open('savefile.txt', 'r') as file:
+            data = file.read()
+            text.delete("1.0", tk.END)  # Clear the Text widget
+            text.insert("1.0", data)
+    except FileNotFoundError:
+        print("No save file found.")
+
+def on_window_close():
+    datas = text.get("1.0", tk.END)
+    with open('savefile.txt', 'w') as file:
+        file.write(datas)
+    root.destroy()
+
+root.protocol("WM_DELETE_WINDOW", on_window_close)
+root.after(100, on_window_load)  # Call after GUI setup
 root.mainloop()
